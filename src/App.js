5632e5cde564633c8635126628e7bbbbb6d7d443
Jeveen Shrestha <home@Jeveens-MacBook-Pro.local>
2024-05-16 15:29:10 +0300

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="" alt="" />
+        <h1>
+          Jeveen Shrestha
+        </h1>
+        <h3>Software Development Engineer</h3>
+        <p>Oulu, Finland</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <div>
+            <img src="" alt="" /><span>jeveen2012shrestha@gmail.com</span>
+          </div>
+          <div>
+            <img src="" alt="" /><span>+358400414879</span>
+          </div>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <main>
+        <section className="profile">
+          <h3>PROFILE</h3>
+          <article>
+            Throughout my career, I have honed my expertise in utilizing modern web technologies, particularly the MERN stack, to create visually stunning and user-friendly web applications. My experience spans various domains, including building dynamic and responsive interfaces for building automation systems and floor heating systems using AngularJS, as well as developing robust and scalable web interfaces for centralized, web-based services using ReactJS with TypeScript. Furthermore, I am currently expanding my skill set by learning Azure cloud technologies, including Azure App Service, Azure Database, Azure Storage, and more. This initiative reflects my commitment to staying abreast of emerging technologies and leveraging them to drive innovation and deliver cutting-edge solutions.
+          </article>
+        </section>
+
+        <section className="experience">
+          <h3>EXPERIENCE</h3>
+          <section>
+            <h2>Software Development Engineer</h2>
+            <h4>Keysight Technology | Oulu, Finland</h4>
+            <h5>March 2022 - Feb 2024</h5>
+            <article>
+              Developed and maintained a web-based service, Nemo Cloud, for remote control and management of measurement fleets using ReactJS, TypeScript, Git, and Docker. Worked in constant collaboration with UX/UI designer, Project and Product managers, Q/A teams, and other products teams.
+            </article>
+          </section>
+          <section>
+            <h2>Software Development Engineer</h2>
+            <h4>Ouman Oy | Oulu, Finland</h4>
+            <h5>Jan 2016 - Feb 2022</h5>
+            <article>
+              Ouman Oy l Linnunrata 14, Kempele Developed and maintained Web Interfaces for building automation system and floor heating system using AngularJS and ReactJS. I too was responsible for UX/UI designing, testing, and bug fixing. Many times worked in constant collaboration with project and product managers, and clients.
+            </article>
+          </section>
+          <section>
+            <h2>Web Development Intern</h2>
+            <h4>Ouman Oy | Oulu, Finland</h4>
+            <h5>Mar 2015 - Dec 2015</h5>
+            <article>
+              Ouman Oy l Linnunrata 14, Kempele Learned about one of the Home Automation Devices, make a UX design and developed an AngularJS application. Tested and fixed bug, and documented.
+            </article>
+          </section>
+        </section>
+
+        <section className="eduction">
+          <h3>EDUCATION</h3>
+          <section>
+            <h2>Bachelors Degree, Information Technology and Communication</h2>
+            <h4>Oulu University of Applied Sciences | Oulu, Finland</h4>
+            <h5>2016</h5>
+          </section>
+          <section>
+            <h2>Intermediate in Science</h2>
+            <h4>Little Angels' College | Lalitpur, Nepal</h4>
+            <h5>2006</h5>
+          </section>
+        </section>
+
+        <section className="thinking">
+          <h3>Thinking</h3>
+          <section>
+            <h2>Analysing | Exploring</h2>
+            <article>
+              I prefer following established and proven approaches when dealing with any obstacles. I appreciate the need for authority and rules and can adjust easily to this. I am generally practical and down to earth but at times may benefit from keeping an open mind to new or novel approaches to problems.
+              I prefer to take decisions based on feelings or instinct rather than rely on evidence. As a result, I tend to pay attention to different views and opinions rather than spending the time analysing data.
+            </article>
+          </section>
+        </section>
+
+        <section className="connecting">
+          <h3>Connecting</h3>
+          <section>
+            <h2>Networking | Collaborating</h2>
+            <article>
+              I display empathy towards colleagues and find it important to listen to their points of view. I am likely to involve others in key decisions and plans. I give credit where it is due and delegate easily when necessary.
+              I am someone who feels at ease when connecting with new people and generally has a well-developed network.
+            </article>
+          </section>
+        </section>
+
+        <section className="executing">
+          <h3>Executing</h3>
+          <section>
+            <h2>Quality | Result driven</h2>
+            <article>
+              I tend to be systematic, methodical and organised and deliver within deadlines. I am reliable and disciplined and driven to achieve the goals.
+              I pay attention to details and enjoy delivering work that is of a high standard.
+            </article>
+          </section>
+        </section>
+
+        <section className="progressing">
+          <h3>Progressing</h3>
+          <section>
+            <h2>Leadership | Resilience | Adaptability</h2>
+            <article>
+              I prefer to work in stable environments and am uncomfortable with dealing with unplanned changes.
+              I am a good team player who pays attention to everyone's input in a group setting. I generally prefer to work in an individual capacity rather than lead teams.
+              I may feel demotivated when faced with negative feedback or setbacks.
+            </article>
+          </section>
+        </section>
+
+      </main>
     </div>
   );
 }
